bbcdccd8594db04186e1cf3262051e7b067cf577
Caleb Smith <caleb.charles.smith23@gmail.com>
2023-01-04 16:34:28 -0600

    Added Commander prefab and movement script

---
+++ ChessGame/Assets/Scripts/Commander.cs
@@ -0,0 +1,119 @@
+using ChessGame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commander : GamePiece
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override bool MoveParameterCheck()
+    {
+        if (NextLocation.TileIndex == TileIndex)
+        {
+            int n = SectorOperations.Subtract(TileSector);
+            Sector i = (Sector)n;
+            while (i != NextLocation.TileSector)
+            {
+                string objectName = i.ToString();
+                objectName += Convert.ToString(TileIndex);
+                GameObject o = GameObject.Find($"{objectName}");
+                TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                if (tile.IsOccupied == true)
+                {
+                    break;
+                }
+                i = (Sector)SectorOperations.Subtract(i);
+            }
+            if (i.Equals(NextLocation.TileSector))
+            {
+                return true;
+            }
+            else
+            {
+                int l = SectorOperations.Add(TileSector);
+                Sector j = (Sector)l;
+                while (j != NextLocation.TileSector)
+                {
+                    string objectName = j.ToString();
+                    objectName += Convert.ToString(TileIndex);
+                    GameObject o = GameObject.Find($"{objectName}");
+                    TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                    if (tile.IsOccupied == true)
+                    {
+                        break;
+                    }
+                    j = (Sector)SectorOperations.Add(j);
+                }
+                if (j.Equals(NextLocation.TileSector))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        else if (NextLocation.TileSector == TileSector)
+        {
+            if (NextLocation.TileIndex < TileIndex)
+            {
+                int i = TileIndex - 1;
+                while (i > NextLocation.TileIndex)
+                {
+                    string objectName = TileSector.ToString();
+                    objectName += Convert.ToString(i);
+                    GameObject o = GameObject.Find($"{objectName}");
+                    TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                    if (tile.IsOccupied == true)
+                    {
+                        return false;
+                    }
+                    i--;
+                }
+                return true;
+            }
+            if (NextLocation.TileIndex > TileIndex)
+            {
+                int i = TileIndex + 1;
+                while (i < NextLocation.TileIndex)
+                {
+                    string objectName = TileSector.ToString();
+                    objectName += Convert.ToString(i);
+                    GameObject o = GameObject.Find($"{objectName}");
+                    TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                    if (tile.IsOccupied == true)
+                    {
+                        return false;
+                    }
+                    i++;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (SectorOperations.DiagonalMove(NextLocation, CurrentLocation))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- ChessGame/Assets/Scripts/SectorEnum.cs
+++ ChessGame/Assets/Scripts/SectorEnum.cs
@@ -39,5 +39,113 @@ namespace ChessGame
                 return Convert.ToInt32(sector + 1);
             }
         }
+
+        public static bool DiagonalMove(TileBehaviour targetLocation, TileBehaviour currentLocation)
+        {
+            bool[] IsValidMove = new bool[4] { true, true, true, true};
+            GameObject tempObject = new GameObject();
+            tempObject.AddComponent<TileBehaviour>();
+            TileBehaviour temp = tempObject.GetComponent<TileBehaviour>();
+            temp.TileIndex = currentLocation.TileIndex;
+            temp.TileSector = currentLocation.TileSector;
+            temp.TileIndex = temp.TileIndex + 1;
+            temp.TileSector = (Sector)SectorOperations.Add(temp.TileSector);
+            while (!(temp.TileSector.Equals(targetLocation.TileSector) && temp.TileIndex.Equals(targetLocation.TileIndex)))
+            {
+                if (temp.TileIndex > 8 || temp.TileIndex < 1)
+                {
+                    IsValidMove[0] = false;
+                    break;
+                }
+                string objectName = $"{temp.TileSector}{temp.TileIndex}";
+                GameObject o = GameObject.Find($"{objectName}");
+                TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                if (tile.IsOccupied == true)
+                {
+                    IsValidMove[0] = false;
+                    break;
+                }
+                temp.TileIndex = temp.TileIndex + 1;
+                temp.TileSector = (Sector)SectorOperations.Add(temp.TileSector);
+            }
+
+            temp.TileIndex = currentLocation.TileIndex;
+            temp.TileSector = currentLocation.TileSector;
+            temp.TileIndex = temp.TileIndex - 1;
+            temp.TileSector = (Sector)SectorOperations.Add(temp.TileSector);
+            while (!(temp.TileSector.Equals(targetLocation.TileSector) && temp.TileIndex.Equals(targetLocation.TileIndex)))
+            {
+                if (temp.TileIndex > 8 || temp.TileIndex < 1)
+                {
+                    IsValidMove[1] = false;
+                    break;
+                }
+                string objectName = $"{temp.TileSector}{temp.TileIndex}";
+                GameObject o = GameObject.Find($"{objectName}");
+                TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                if (tile.IsOccupied == true)
+                {
+                    IsValidMove[1] = false;
+                    break;
+                }
+                temp.TileIndex = temp.TileIndex - 1;
+                temp.TileSector = (Sector)SectorOperations.Add(temp.TileSector);
+            }
+
+            temp.TileIndex = currentLocation.TileIndex;
+            temp.TileSector = currentLocation.TileSector;
+            temp.TileIndex = temp.TileIndex - 1;
+            temp.TileSector = (Sector)SectorOperations.Subtract(temp.TileSector);
+            while (!(temp.TileSector.Equals(targetLocation.TileSector) && temp.TileIndex.Equals(targetLocation.TileIndex)))
+            {
+                if (temp.TileIndex > 8 || temp.TileIndex < 1)
+                {
+                    IsValidMove[2] = false;
+                    break;
+                }
+                string objectName = $"{temp.TileSector}{temp.TileIndex}";
+                GameObject o = GameObject.Find($"{objectName}");
+                TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                if (tile.IsOccupied == true)
+                {
+                    IsValidMove[2] = false;
+                    break;
+                }
+                temp.TileIndex = temp.TileIndex - 1;
+                temp.TileSector = (Sector)SectorOperations.Subtract(temp.TileSector);
+            }
+
+            temp.TileIndex = currentLocation.TileIndex;
+            temp.TileSector = currentLocation.TileSector;
+            temp.TileIndex = temp.TileIndex + 1;
+            temp.TileSector = (Sector)SectorOperations.Subtract(temp.TileSector);
+            while (!(temp.TileSector.Equals(targetLocation.TileSector) && temp.TileIndex.Equals(targetLocation.TileIndex)))
+            {
+                if (temp.TileIndex > 8 || temp.TileIndex < 1)
+                {
+                    IsValidMove[3] = false;
+                    break;
+                }
+                string objectName = $"{temp.TileSector}{temp.TileIndex}";
+                GameObject o = GameObject.Find($"{objectName}");
+                TileBehaviour tile = o.GetComponent("TileBehaviour") as TileBehaviour;
+                if (tile.IsOccupied == true)
+                {
+                    IsValidMove[3] = false;
+                    break;
+                }
+                temp.TileIndex = temp.TileIndex + 1;
+                temp.TileSector = (Sector)SectorOperations.Subtract(temp.TileSector);
+            }
+
+            if (IsValidMove[0] || IsValidMove[1] || IsValidMove[2] || IsValidMove[3] )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
