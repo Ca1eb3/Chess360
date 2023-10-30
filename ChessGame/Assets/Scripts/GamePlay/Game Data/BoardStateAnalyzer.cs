a6c4cd8748d23eaf4a4b4117e293d25486e4f35b
Caleb Smith <caleb.charles.smith23@gmail.com>
2023-10-30 17:32:43 -0500

    Create methods to update the board evaluation and the ui

--- ChessGame/Assets/Scripts/GamePlay/Game Data/BoardStateAnalyzer.cs
+++ ChessGame/Assets/Scripts/GamePlay/Game Data/BoardStateAnalyzer.cs
@@ -30,6 +30,38 @@ public class BoardStateAnalyzer
         CalculateBoardEvaluation();
     }
 
+    public void UpdateBoardState(GamePiece piece, BitArray checkControl)
+    {
+        // procedure:
+        // update 4 control bits
+        // set the 5 bit sequence for the piece's current location equal to the 5 bit sequence for it's previous location
+        // set the 5 bit sequence for the piece's previous location equal to 00000
+        // flips the turn bit
+        BoardState.Set(0, !BoardState.Get(0));
+        // update the check control bits
+        for (int i = 1; i < 4; i++)
+        {
+            BoardState.Set(i, checkControl.Get(i - 1));
+        }
+        // index the previous location
+        int indexingPrev = 4;
+        indexingPrev += (int)piece.PreviousLocation.TileSector * 40;
+        indexingPrev += (piece.PreviousLocation.TileIndex - 1) * 5;
+
+        // index the current location
+        int indexingCur = 4;
+        indexingCur += (int)piece.TileSector * 40;
+        indexingCur += (piece.TileIndex - 1) * 5;
+
+        // update the current location and previous location of the moved piece
+        for (int i = 0; i < 5; i++)
+        {
+            BoardState.Set(indexingPrev + i, false);
+            BoardState.Set(indexingCur + i, Convert.ToBoolean(piece.BitValue.Get(0 + i)));
+        }
+        CalculateBoardEvaluation();
+    }
+
     public void CalculateBoardEvaluation()
     {
         BoardEvaluation = 0;
@@ -60,12 +92,10 @@ public class BoardStateAnalyzer
             if (BoardState[3] == true)
             {
                 BoardEvaluation -= .2;
-                return;
             }
             else
             {
                 BoardEvaluation += .2;
-                return;
             }
         }
         for (int i = 4; i < 484; i = i + 5)
--- ChessGame/Assets/Scripts/GamePlay/Game Data/GameData.cs
+++ ChessGame/Assets/Scripts/GamePlay/Game Data/GameData.cs
@@ -28,6 +28,7 @@ public class GameData : MonoBehaviour
     public bool IsOver = false;
     public PieceColor Winner = PieceColor.None;
     public TextMeshProUGUI MoveRecordText;
+    public TextMeshProUGUI EvaluationRecordText;
     public GamePiece RemovedPiece;
     public bool CheckBlack = false;
     public bool CheckWhite = false;
@@ -45,9 +46,11 @@ public class GameData : MonoBehaviour
     void Start()
     {
         UpdateGamePieces();
-        MoveRecordText = GameObject.Find("MoveRecordContent").GetComponent<TextMeshProUGUI>().ConvertTo<TextMeshProUGUI>();
+        MoveRecordText = GameObject.Find("MoveRecordContent").GetComponent<TextMeshProUGUI>();
+        EvaluationRecordText = GameObject.Find("BoardEvaluationContent").GetComponent<TextMeshProUGUI>();
         UpdateOverseers();
         BoardStateAnalyzer.SetUp(GamePieces);
+        UpdateBoardEvaluation();
     }
     
     // Update is called once per frame
@@ -188,6 +191,7 @@ public class GameData : MonoBehaviour
         SelectedPiece.TileSector = SelectedPiece.NextLocation.TileSector;
         SelectedPiece.TileIndex = SelectedPiece.NextLocation.TileIndex;
         // Update piece location
+        SelectedPiece.PreviousLocation = SelectedPiece.CurrentLocation;
         SelectedPiece.CurrentLocation = SelectedPiece.NextLocation;
         SelectedPiece.NextLocation = null;
         // update original tile and new tile
@@ -223,8 +227,38 @@ public class GameData : MonoBehaviour
             IsOver = true;
             Winner = PieceColor.None;
         }
+        // Prepare control bit values and calculate board state
+        BitArray CheckControl = new BitArray(3);
+        if (Checkmate == true)
+        {
+            if (Winner == PieceColor.White)
+            {
+                CheckControl.Set(0, true);
+            }
+            else
+            {
+                CheckControl.Set(0, true);
+                CheckControl.Set(2, true);
+            }
+        }
+        else if (CheckBlack == true)
+        {
+            CheckControl.Set(1, true);
+            CheckControl.Set(2, true);
+        }
+        else if (CheckWhite == true)
+        {
+            CheckControl.Set(1, true);
+        }
+        else if (IsOver == true)
+        {
+            CheckControl.Set(2, true);
+        }
+
+        BoardStateAnalyzer.UpdateBoardState(SelectedPiece, CheckControl);
         // Update Move Record
         UpdateMoveRecord();
+        UpdateBoardEvaluation();
         ResetStatus();
     }
 
@@ -237,6 +271,11 @@ public class GameData : MonoBehaviour
         // reset attacking pieces
         AttackingBlack.Clear();
         AttackingWhite.Clear();
+    }
+
+    public void UpdateBoardEvaluation()
+    {
+        EvaluationRecordText.text = "Piece Evaluation: " + BoardStateAnalyzer.PieceEvaluation.ToString() +"\r\n\r\nBoard Evaluation: " + BoardStateAnalyzer.BoardEvaluation.ToString();
     }
 
     public void UpdateMoveRecord()
